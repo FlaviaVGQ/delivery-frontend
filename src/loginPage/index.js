0bc11ae649294b2e594178014f40d3662d372ea9
helaniobf <81391610+helaniobf@users.noreply.github.com>
2024-11-04 02:23:32 -0300

    Melhoria na página de login

--- src/loginPage/index.js
+++ src/loginPage/index.js
@@ -36,17 +36,13 @@ const LoginPage = () => {
 
             if (response.success) {
                 setMessage('Login realizado com sucesso!');
-                console.log('Usuário:', response.username);
-                localStorage.setItem('username', response.username); 
+                localStorage.setItem('username', response.username);
                 localStorage.setItem('userId', response.userId);
-                const userId = localStorage.getItem('userId');
-                console.log('ID do usuário:', userId); 
                 navigate('/home');
             } else if (response.remaining_time !== undefined) {
                 setRemainingTime(response.remaining_time);
                 setMessage(`Conta bloqueada.`);
             } else {
-
                 if (response.message.includes('Usuário não encontrado')) {
                     setMessage('Usuário não encontrado. Verifique o nome de usuário e tente novamente.');
                 } else if (response.message.includes('Senha incorreta')) {
@@ -61,48 +57,50 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="login-container">
-            <div className="login-box">
-                <img src="/logo.png" alt="Logo" className="login-logo" />
-                <form onSubmit={handleSubmit} className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="username"><FaUser /> Usuário</label>
-                        <input
-                            type="text"
-                            id="username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                            className="input-field"
-                            placeholder="Digite seu usuário"
-                        />
+        <div className="login-page">
+            <div className="login-container">
+                <div className="login-box">
+                    <img src="/logo.png" alt="Logo" className="login-logo" />
+                    <form onSubmit={handleSubmit} className="login-form">
+                        <div className="form-group">
+                            <label htmlFor="username"><FaUser /> Usuário</label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                className="input-field"
+                                placeholder="Digite seu usuário"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password"><FaLock /> Senha</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                className="input-field"
+                                placeholder="Digite sua senha"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="login-button"
+                            disabled={remainingTime > 0}
+                        >
+                            {remainingTime > 0
+                                ? `Tente novamente em ${formatTime(remainingTime)}`
+                                : 'Entrar'}
+                        </button>
+                    </form>
+                    {message && <p className="login-message">{message}</p>}
+                    <div className="return-login">
+                        <p>Não tem uma conta? <Link to="/createUser">Criar conta</Link></p>
+                        <p><Link to="/forgotPassword">Esqueci minha senha</Link></p>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="password"><FaLock /> Senha</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="input-field"
-                            placeholder="Digite sua senha"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="login-button"
-                        disabled={remainingTime > 0}
-                    >
-                        {remainingTime > 0
-                            ? `Tente novamente em ${formatTime(remainingTime)}`
-                            : 'Entrar'}
-                    </button>
-                </form>
-                {message && <p className="login-message">{message}</p>}
-                <div className="return-login">
-                    <p>Não tem uma conta? <Link to="/createUser">Criar conta</Link></p>
-                    <p><Link to="/forgotPassword">Esqueci minha senha</Link></p>
                 </div>
             </div>
         </div>
