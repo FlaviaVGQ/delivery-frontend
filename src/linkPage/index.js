eabb28eccd6a30dae283cbdbf9e48950e5728305
LuizaLLeite <113855143+LuizaLLeite@users.noreply.github.com>
2025-06-26 22:50:17 -0300

    Update: adicionar suporte ao modo escuro em linkPage index.js

--- src/linkPage/index.js
+++ src/linkPage/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import {Link } from 'react-router-dom';
-import {FaUserCircle, FaSignOutAlt, FaClipboard, FaHome} from 'react-icons/fa';
+import {FaUserCircle, FaSignOutAlt, FaClipboard, FaHome, FaSun, FaMoon} from 'react-icons/fa';
 import './linkpage.css';
 
 const LinkPage = () => {
     const [menuLink, setMenuLink] = useState('');
     const [copied, setCopied] = useState(false);
     const userId = localStorage.getItem('userId');
+    const [darkMode, setDarkMode] = useState(false);
+
 
     useEffect(() => {
         if (userId) {
@@ -22,20 +24,32 @@ const LinkPage = () => {
                 .catch(err => console.error('Erro ao copiar o link: ', err));
         }
     };
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
 
     return (
-        <div className="create-link-page">
+        <div className={`admin-homepage-container ${darkMode ? 'dark' : ''}`}>
             <header className="admin-homepage-header">
                 <img src="/logo.png" alt="Logo" className="admin-homepage-logo" />
                 <nav className="admin-homepage-nav">
                     <ul className="nav-list">
-                        <li><Link to="/restaurante"><FaUserCircle/> Perfil</Link></li>
-                        <li><Link to="/home"><FaHome/> Início</Link></li>
-                        <li><Link to="/" className="logout-button"><FaSignOutAlt/> Sair</Link></li>
+                        <li><Link to="/restaurante"><FaUserCircle /> Perfil</Link></li>
+                        <li><Link to="/home"><FaHome /> Início</Link></li>
+                        <li><Link to="/" className="logout-button"><FaSignOutAlt /> Sair</Link></li>
+                        <li>
+                            <button
+                                onClick={toggleDarkMode}
+                                className="dark-mode-toggle"
+                                aria-label="Toggle dark mode"
+                            >
+                                {darkMode ? <FaSun /> : <FaMoon />}
+                            </button>
+                        </li>
                     </ul>
                 </nav>
             </header>
-
             <main className="create-link-main">
                 <div className="form-container">
                     <h1>Link do Cardápio</h1>
